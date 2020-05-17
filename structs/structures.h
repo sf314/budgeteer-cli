1a28d6801d103960ed5995185f47bf1be071c499
Stephen Flores <sf314@icloud.com>
2020-05-17 13:19:32 -0600

    Add basic structure definitions
    transaction_t
    budget_t

---
+++ structs/structures.h
@@ -0,0 +1,22 @@
+#ifndef _STRUCTURES_H
+#define _STRUCTURES_H
+
+#include <unistd.h>
+
+#define DESCRIPTION_LEN 100
+#define NAME_LEN 20
+#define MAX_TRANSACTIONS 100
+
+typedef struct __attribute__((packed)) {
+    uint64_t timestamp;
+    double amount;
+    char description[DESCRIPTION_LEN];
+} transaction_t; 
+
+typedef struct __attribute__((packed)) {
+    double limit;
+    transaction_t transactions[MAX_TRANSACTIONS];
+    char name[NAME_LEN];
+} budget_t;
+
+#endif
